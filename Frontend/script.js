3f6659efdccd57ba2f1a01c9542b6b84c483a598
miakahira <miastraumann@icloud.com>
2026-04-21 13:57:47 +0200

    unnötiges löschen (frag_Mia Button)

--- Frontend/script.js
+++ Frontend/script.js
@@ -12,21 +12,6 @@ imageInput.addEventListener('change', (event) => {
     console.log('Ausgewählte Datei:', file);
 });
 
-// Funktion, um Mia zu fragen
-function askMia() {
-    console.log('Mia wird gefragt...');
-
-    fetch('http://localhost:3000/mia')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Antwort von Mia:', data);
-            alert(data.message); // Zeigt die Antwort in einem Alert an
-        })
-        .catch(error => {
-            console.error('Fehler beim Abrufen von Mia:', error);
-        });
-}
-
 
 function patterns() {
     console.log('Patterns werden geladen...');
